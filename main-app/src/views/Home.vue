9d41efd603cf245f4d15510626a0e14ca4420a9c
陈靖 <chenj@isyscore.com>
2021-04-27 17:11:16 +0800

    feat: 添加 以组件的方式使用微应用 交互效果

--- main-app/src/views/Home.vue
+++ main-app/src/views/Home.vue
@@ -1,24 +1,56 @@
 <!--
  * @Author: jing.chen
  * @Date: 2020-10-29 16:57:52
- * @LastEditors: jing.chen
- * @LastEditTime: 2020-10-29 17:25:42
+ * @LastEditors: Please set LastEditors
+ * @LastEditTime: 2021-04-27 17:10:05
  * @Description: 
 -->
 <template>
   <div class="home">
+    <h1>点击上方tab 切换子应用</h1>
     <h1>这里是父应用，请运行子应用1，2后再点击tab才有效果</h1>
+    <br />
+    <br />
+    <span>点击上方tab是通过路由 去切换子项目</span>
+    <br />
+    <span>如果我只有部分数据需要子项目，例如弹窗之类的，该如何实现呢？</span>
+    <br />
+    <el-button type="primary" @click="openMicro1">应用1弹窗</el-button>
+
+    <el-dialog
+      title="不切路由 父应用内打开 子应用1"
+      v-if="dialogVisible"
+      :visible.sync="dialogVisible"
+      width="50%"
+      :before-close="() => dialogVisible = false">
+      <micro-app></micro-app>
+      <span slot="footer" class="dialog-footer">
+        <el-button @click="dialogVisible = false">取 消</el-button>
+        <el-button type="primary" @click="dialogVisible = false">确 定</el-button>
+      </span>
+    </el-dialog>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import HelloWorld from '@/components/HelloWorld.vue'
-
+import MicroApp from './microApp.vue'
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+    HelloWorld,
+    MicroApp
+  },
+  data () {
+    return {
+      dialogVisible: false
+    }
+  },
+  methods: {
+    openMicro1 () {
+      this.dialogVisible = true
+    }
   }
 }
 </script>
---
+++ main-app/src/views/microApp.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="microApp">
+    <div id="child1"></div>
+  </div>
+</template>
+
+<script>
+// https://www.yuque.com/kuitos/gky7yw/uyp6wi
+import { loadMicroApp } from 'qiankun';
+export default {
+  name: 'microApp',
+
+  data () {
+    return {
+      microApp: null
+    }
+  },
+
+  mounted () {
+    // const container = document.
+    this.microApp = loadMicroApp({ name: '子应用child01', container: '#child1', entry: 'http://localhost:9010' });
+    
+  },
+  beforeDestroy () {
+    this.microApp.unmount();
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
