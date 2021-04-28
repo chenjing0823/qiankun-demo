d45b8994fb87d94aa6fde9c0e8c01e47c3c1a5c1
陈靖 <chenj@isyscore.com>
2021-04-28 14:08:22 +0800

    feat: 使用ref

--- main-app/src/views/microApp.vue
+++ main-app/src/views/microApp.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="microApp">
-    <div id="child1"></div>
+    <!-- <div id="child1"></div> -->
+    <div ref="child1"></div>
   </div>
 </template>
 
@@ -18,7 +19,8 @@ export default {
 
   mounted () {
     // const container = document.
-    this.microApp = loadMicroApp({ name: '子应用child01', container: '#child1', entry: 'http://localhost:9010' });
+    // this.microApp = loadMicroApp({ name: '子应用child01', container: '#child1', entry: 'http://localhost:9010' });
+    this.microApp = loadMicroApp({ name: '子应用child01', container: this.$refs.child1, entry: 'http://localhost:9010' });
     
   },
   beforeDestroy () {
